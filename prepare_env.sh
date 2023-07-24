ad69f1a0a9a18b48056b6e0e2b0617bba8f649d6
bohanLee <bohan@bupt.edu.cn>
2023-07-24 19:33:36 +0800

    new_ops

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,25 +1,31 @@
+pip3 install numpy
+pip3 install torch
+pip3 install torchvision
+pip3 install opencv-python
+pip3 install Cython
+pip3 install numba
+pip3 install progress
+pip3 install matplotlib
+pip3 install easydict
+pip3 install scipy
+pip3 install pillow==6.2.1
+pip3 install scikit-image
+
+
 THIS_DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 cd $THIS_DIR
-tar zxvf torch12.tar.gz
 
-cd $THIS_DIR
 CURRENT_DIR=$(pwd)
 
 # cd $CURRENT_DIR
 # source prepare_env.sh
 cd $CURRENT_DIR/cocoapi/PythonAPI
 make
-$CURRENT_DIR/torch12/bin/python setup.py install --user
+python3 setup.py install --user
 
 
 cd $CURRENT_DIR/CrowdPose/crowdpose-api/PythonAPI
 make install
-$CURRENT_DIR/torch12/bin/python setup.py install --user
+python3 setup.py install --user
 
 
-cd $CURRENT_DIR/src/lib/models/networks/DCNv2
-$CURRENT_DIR/torch12/bin/python setup.py build develop
-cd $CURRENT_DIR/src/lib/external
-make
-cd $CURRENT_DIR/src/lib/models/resample2d_package
-$CURRENT_DIR/torch12/bin/python setup.py install --user
